6ffcaf409d40b4804bcf60c4ddea2410a2c8cac9
zoranbogoevski <zoran@orangemelon.com>
2024-02-23 13:56:27 +0100

    Add some new features

--- config/rssfeed.php
+++ config/rssfeed.php
@@ -1,9 +1,15 @@
 <?php
+
 return [
-    'content_element_xpaths' => [
-        '//div[@class="post-content"]',
-        '//div[@class="article-body"]',
-        '//div[@class="td-post-content"]',
-        '//div[contains(concat(" ", normalize-space(@class), " "), " post-single-content ") and contains(concat(" ", normalize-space(@class), " "), " box ") and contains(concat(" ", normalize-space(@class), " "), " mark-links ")]',
+    'domain_xpaths' => [
+        [
+            'domain' => 'mistagogia.mk',
+            'content_element_xpaths' => [
+                '//div[@class="single_post"]',
+            ],
+        ],
     ],
+    'min_image_width' => 300,
+    'image_storage_path' => 'images',
 ];
+
--- src/RssFeed.php
+++ src/RssFeed.php
@@ -46,7 +46,7 @@ class RssFeed implements ShouldQueue
                 $fullContent = $this->retrieveFullContent($itemLink);
 
                 // Save the image to storage
-                $images = $this->saveImageToStorage($fullContent);
+                $images = $this->saveImagesToStorage($fullContent['images']);
 
                 // Add the extracted item data to the parsedItems array
                 $parsedItems[] = [
@@ -71,15 +71,16 @@ class RssFeed implements ShouldQueue
      * @return array|bool
      * @throws CantOpenFileFromUrlException
      */
-    public function saveImageToStorage(array $images): array
+    public function saveImagesToStorage(array $images): array
     {
         $savedImageNames = [];
+        $imageStoragePath = config('rssfeed.image_storage_path', 'images');
 
         foreach ($images as $image) {
-                $file = UrlUploadedFile::createFromUrl($image);
-                $imageName = Str::random(15) . '.' . $file->extension();
-                $file->storeAs('images', $imageName, 'public');
-                $savedImageNames[] = $imageName;
+            $file = UrlUploadedFile::createFromUrl($image);
+            $imageName = Str::random(15) . '.' . $file->extension();
+            $file->storeAs($imageStoragePath, $imageName, 'public');
+            $savedImageNames[] = $imageName;
 
         }
 
@@ -92,54 +93,54 @@ class RssFeed implements ShouldQueue
      */
     public function retrieveFullContent(string $postLink): bool|array
     {
-        // Fetch the HTML content using cURL
-        $html = $this->fetchContentUsingCurl($postLink); // Use the previously defined cURL fetching function
-
-        if ($html === false) {
-            return false; // Handle the error as appropriate
-        }
-
-        // Load the HTML content into DOMDocument
+        $html = $this->fetchContentUsingCurl($postLink);
+        $parsedUrl = parse_url($postLink);
+        $host = $parsedUrl['host'] ?? '';
+        $domains = config('rssfeed.domain_xpaths');
+        $contentElementXPaths = collect($domains)->firstWhere('domain', $host)['content_element_xpaths'];
         $dom = new DOMDocument();
         @$dom->loadHTML($html);
-
-        // Use DOMXPath to work with the DOM
         $xpath = new DOMXPath($dom);
 
-        // Initialize an array to hold the image URLs
         $imageUrls = [];
         $selectedContent = '';
+        $minImageWidth = config('rssfeed.min_image_width', 600); // Retrieve the minimum image width from config
 
-        // Process each XPath query in the configuration
-        foreach ($config['content_element_xpaths'] as $xpathQuery) {
+        foreach ($contentElementXPaths as $xpathQuery) {
             $elements = $xpath->query($xpathQuery);
-
-            // Check if elements were found for the current XPath query
             if ($elements->length > 0) {
                 foreach ($elements as $element) {
-                    // Extract and concatenate the HTML of each matching element
                     $selectedContent .= $dom->saveHTML($element);
-
-                    // Find and store all <img> tags within the current element
                     $images = $xpath->query('.//img', $element);
                     foreach ($images as $img) {
                         $src = $img->getAttribute('src');
-                        $imageUrls[] = $src;
+                        // Get image dimensions
+                        list($width, $height) = getimagesize($src);
+                        if ($width >= $minImageWidth) {
+                            // Add to array if not already added
+                            if (!in_array($src, $imageUrls)) {
+                                $imageUrls[] = $src;
+                            }
+                        }
                     }
                 }
             }
         }
 
-        // Optionally, you might want to remove <script> and <style> from $selectedContent
         $selectedContent = preg_replace('/<script\b[^>]*>(.*?)<\/script>/is', "", $selectedContent);
         $selectedContent = preg_replace('/<style\b[^>]*>(.*?)<\/style>/is', "", $selectedContent);
 
-        // Return the content and images
         return [
             'content' => trim($selectedContent),
-            'images' => $imageUrls, // This is an array of image URLs found in the selected content
+            'images' => $imageUrls,
         ];
     }
+
+
+
+
+
+// The cURL fetching function from previous examples
     private function fetchContentUsingCurl(string $url): bool|string
     {
         $ch = curl_init();
@@ -153,4 +154,6 @@ class RssFeed implements ShouldQueue
 
         return $httpCode === 200 ? $data : false;
     }
+
+
 }
