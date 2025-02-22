dde632537b78002d746076e0287d4fbc02c51e68
KalimeroMK <zbogoevski@gmail.com>
2025-02-22 01:01:54 +0100

    refactor RssFeedTest to improve test structure and add missing tests

--- Tests/Unit/RssFeedTest.php
+++ Tests/Unit/RssFeedTest.php
@@ -38,85 +38,83 @@ class RssFeedTest extends TestCase
      * of SimplePie and that the title of the feed is a non-empty string.
      *
      * @return void
-     * @throws CantOpenFileFromUrlException
      */
     public function test_it_can_parse_rss_feeds(): void
     {
         $url = 'https://www.tiveriopol.mk/feed/';
-        $rss = $this->rssFeed->parseRssFeeds($url);
-        dd($rss);
+        $rss = $this->rssFeed->RssFeeds($url);
         $this->assertInstanceOf(SimplePie::class, $rss);
         $this->assertIsString($rss->get_title());
         $this->assertNotEmpty($rss->get_title());
     }
 
-//    /** @test */
-//    public function it_throws_exception_when_cannot_open_rss_feed(): void
-//    {
-//        $this->expectException(CantOpenFileFromUrlException::class);
-//
-//        $url = 'https://invalid-url.com/rss.xml';
-//
-//        Http::fake([
-//            $url => Http::response('', 404),
-//        ]);
-//
-//        $this->rssFeed->parseRssFeeds($url);
-//    }
-//
-//    /** @test */
-//    public function it_can_check_if_url_exists(): void
-//    {
-//        $validUrl = 'http://mistagogia.mk/feed';
-//        $invalidUrl = 'https://example.com/invalid-url';
-//
-//        Http::fake([
-//            $validUrl => Http::response('Valid response', 200),
-//            $invalidUrl => Http::response('', 404),
-//        ]);
-//
-//        $this->assertTrue($this->rssFeed->urlExists($validUrl));
-//        $this->assertFalse($this->rssFeed->urlExists($invalidUrl));
-//
-//    }
-//
-//    /** @test */
-//    public function it_extracts_image_from_description()
-//    {
-//        $description = '<p>Some text <img src="https://example.com/image.jpg" /> more text</p>';
-//
-//        $rssFeed = new RssFeed(app());
-//        $imageUrl = $rssFeed->extractImageFromDescription($description);
-//
-//        $this->assertEquals('https://example.com/image.jpg', $imageUrl);
-//    }
-//
-//    /** @test */
-//    public function it_returns_null_if_no_image_in_description()
-//    {
-//        $description = '<p>Some text without an image</p>';
-//
-//        $rssFeed = new RssFeed(app());
-//        $imageUrl = $rssFeed->extractImageFromDescription($description);
-//
-//        $this->assertNull($imageUrl);
-//    }
-//    public function it_infers_extension_from_mime_type()
-//    {
-//        $imageUrls = [
-//            'https://example.com/image_without_extension',
-//        ];
-//
-//        Http::fake([
-//            'https://example.com/image_without_extension' => Http::response('image content', 200, ['Content-Type' => 'image/jpeg']),
-//        ]);
-//
-//        $rssFeed = new RssFeed(app());
-//        $savedImages = $rssFeed->saveImagesToStorage($imageUrls);
-//
-//        $this->assertCount(1, $savedImages);
-//        $this->assertStringEndsWith('.jpg', $savedImages[0]);
-//        Storage::disk('public')->assertExists('images/' . $savedImages[0]);
-//    }
+    /** @test */
+    public function it_throws_exception_when_cannot_open_rss_feed(): void
+    {
+        $this->expectException(CantOpenFileFromUrlException::class);
+
+        $url = 'https://invalid-url.com/rss.xml';
+
+        Http::fake([
+            $url => Http::response('', 404),
+        ]);
+
+        $this->rssFeed->RssFeeds($url);
+    }
+
+    /** @test */
+    public function it_can_check_if_url_exists(): void
+    {
+        $validUrl = 'http://mistagogia.mk/feed';
+        $invalidUrl = 'https://example.com/invalid-url';
+
+        Http::fake([
+            $validUrl => Http::response('Valid response', 200),
+            $invalidUrl => Http::response('', 404),
+        ]);
+
+        $this->assertTrue($this->rssFeed->urlExists($validUrl));
+        $this->assertFalse($this->rssFeed->urlExists($invalidUrl));
+
+    }
+
+    /** @test */
+    public function it_extracts_image_from_description()
+    {
+        $description = '<p>Some text <img src="https://example.com/image.jpg" /> more text</p>';
+
+        $rssFeed = new RssFeed(app());
+        $imageUrl = $rssFeed->extractImageFromDescription($description);
+
+        $this->assertEquals('https://example.com/image.jpg', $imageUrl);
+    }
+
+    /** @test */
+    public function it_returns_null_if_no_image_in_description()
+    {
+        $description = '<p>Some text without an image</p>';
+
+        $rssFeed = new RssFeed(app());
+        $imageUrl = $rssFeed->extractImageFromDescription($description);
+
+        $this->assertNull($imageUrl);
+    }
+    public function it_infers_extension_from_mime_type()
+    {
+        $imageUrls = [
+            'https://example.com/image_without_extension',
+        ];
+
+        Http::fake([
+            'https://example.com/image_without_extension' => Http::response('image content', 200, ['Content-Type' => 'image/jpeg']),
+        ]);
+
+        $rssFeed = new RssFeed(app());
+        $savedImages = $rssFeed->saveImagesToStorage($imageUrls);
+
+        $this->assertCount(1, $savedImages);
+        $this->assertStringEndsWith('.jpg', $savedImages[0]);
+        Storage::disk('public')->assertExists('images/' . $savedImages[0]);
+    }
 
 }
